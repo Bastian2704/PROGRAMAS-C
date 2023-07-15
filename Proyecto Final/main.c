77ca0e5ebd2a75e27e55cac5bca1d1dcd4ba5041
Bastian2704 <80728908+Bastian2704@users.noreply.github.com>
2023-07-15 18:02:19 -0500

    Update main.c

--- Proyecto Final/main.c
+++ Proyecto Final/main.c
@@ -1,59 +1,65 @@
 #include <stdio.h>
 #include <string.h>
+//El código se actualizó con pequeñas modificaciones estéticas
 
 int menuPrincipal();
 void crearArchivo(char nombreArchivo[]);
-void guardarNombresArchivo(char nombreArchivo[], char ID[7][5][50]);
-int verProductos(char ID[7][5][50],int var);
-int nuevoProducto(FILE *archivo, char ID[7][5][50],int var);
-int modificarProductos(FILE *archivo, char ID[7][5][50]);
+void guardarNombresArchivo(char nombreArchivo[], char ID[6][5][50]);
+int verProductos(char ID[6][5][50], int var);
+int nuevoProducto(FILE *archivo, char ID[6][5][50], char nombreArchivo[]);
+int modificarProductos(FILE *archivo, char ID[6][5][50], char nombreArchivo[]);
+int eliminarProducto(FILE *archivo, char ID[6][5][50], int var, char nombreArchivo);
 
 int main(int argc, char *argv[])
 {
     int menu;
-    char ID[7][5][50] = {
+    char ID[6][5][50] = {
         {"01", "Lambor", "Huracan", "3", "1.5"},
         {"02", "Porshe", "911 GT", "8", "1"},
-        {"03", "Pagani", "Utopia", "2", "2.7"},
+        {"03", "Pagani", "Utopia", "2", "2.6"},
         {"04", "AstonM", "Valky", "1", "3.2"},
         {"05", "Ferrari", "Enzo", "4", "4.2"},
-        {"06", "", "", "", ""}, 
-        {"07", "", "", "", ""}  
+        {"06", "", "", "", ""},
     };
 
     char nombreArchivo[20] = "productos.txt";
     FILE *archivo;
+
     archivo = fopen(nombreArchivo, "r+");
-    int var=5;
+    int var = 5;
 
     if (archivo == NULL)
     {
         crearArchivo(nombreArchivo);
         guardarNombresArchivo(nombreArchivo, ID);
     }
-
+else{
     do
     {
         menu = menuPrincipal();
         switch (menu)
         {
         case 1:
-            menu = verProductos(ID,var);
+            menu = verProductos(ID, var);
             break;
         case 2:
-            menu = nuevoProducto(archivo, ID, var);
+            menu = nuevoProducto(archivo, ID, nombreArchivo);
             var++;
             break;
         case 3:
-            menu = modificarProductos(archivo, ID);
+            menu = modificarProductos(archivo, ID, nombreArchivo);
+            break;
+        case 4:
+            menu = eliminarProducto(archivo, ID, var, nombreArchivo);
+            var--;
             break;
         default:
             printf("No se ingresó ninguna opción válida.\n");
             menu = 0;
             break;
         }
-    } while (menu == 0);
-
+    } while (menu !=5);
+}
     fclose(archivo);
     return 0;
 }
@@ -62,7 +68,7 @@ int menuPrincipal()
 {
     int menu;
     printf("--------------------------------------INVENTARIO--------------------------------------\n");
-    printf("1.-VER PRODUCTOS  \t 2.-INGRESAR PRODUCTOS \t\t 3.-MODIFICACION DE PRODUCTOS\n");
+    printf("1.-VER PRODUCTOS  \t 2.-INGRESAR PRODUCTOS \t\t 3.-MODIFICACION DE PRODUCTOS\t4.-ELIMINAR PRODUCTO\n");
     scanf("%d", &menu);
     return menu;
 }
@@ -83,7 +89,7 @@ void crearArchivo(char nombreArchivo[])
     }
 }
 
-void guardarNombresArchivo(char nombreArchivo[], char ID[7][5][50])
+void guardarNombresArchivo(char nombreArchivo[], char ID[6][5][50])
 {
     FILE *archivo;
     archivo = fopen(nombreArchivo, "r+");
@@ -93,7 +99,7 @@ void guardarNombresArchivo(char nombreArchivo[], char ID[7][5][50])
     }
     else
     {
-        for (int i = 0; i < 7; i++) // Corregido el límite del bucle
+        for (int i = 0; i < 6; i++)
         {
             fprintf(archivo, "%s %s %s %s %s\n", ID[i][0], ID[i][1], ID[i][2], ID[i][3], ID[i][4]);
         }
@@ -101,7 +107,7 @@ void guardarNombresArchivo(char nombreArchivo[], char ID[7][5][50])
     }
 }
 
-int verProductos(char ID[7][5][50],int var)
+int verProductos(char ID[6][5][50], int var)
 {
     FILE *archivo;
     archivo = fopen("productos.txt", "r");
@@ -110,7 +116,7 @@ int verProductos(char ID[7][5][50],int var)
     printf("-------\tPRODUCTOS\t-------\n");
     printf("ID   NOMBRE\t\tDESCRIPCION\tCANTIDAD\tPRECIO\n");
 
-    for (int i = 0; i < var; i++) 
+    for (int i = 0; i < var; i++)
     {
         printf("%s\t%s\t\t%s\t\t%s\t\t\t%s M\n", ID[i][0], ID[i][1], ID[i][2], ID[i][3], ID[i][4]);
     }
@@ -121,82 +127,99 @@ int verProductos(char ID[7][5][50],int var)
     return menu;
 }
 
-int nuevoProducto(FILE *archivo, char ID[7][5][50],int var)
+int nuevoProducto(FILE *archivo, char ID[6][5][50], char nombreArchivo[])
 {
     int menu;
-    int i = var; 
+    int i=5;
     printf("Ingrese el nuevo producto\n");
     printf("Nombre:");
-    scanf("%s", ID[i][1]); 
+    scanf("%s", ID[i][1]);
     printf("Descripcion:");
-    scanf("%s", ID[i][2]); 
+    scanf("%s", ID[i][2]);
     printf("Cantidad:");
-    scanf("%s", ID[i][3]); 
+    scanf("%s", ID[i][3]);
     printf("Precio: ");
-    scanf("%s", ID[i][4]); 
+    scanf("%s", ID[i][4]);
 
     fseek(archivo, 0, SEEK_END);
-    ftell(archivo) / (sizeof(float) * 7 + sizeof(char) * 30); 
+    ftell(archivo) / (sizeof(float) * 6 + sizeof(char) * 30);
 
-    fprintf(archivo, "%.0f %.0f %.1f %s %s\n", atof(ID[i][0]), atof(ID[i][1]), atof(ID[i][2]), atof(ID[i][3]), atof(ID[i][4])); 
-
+    fprintf(archivo, "%.0f %.0f %.1f %s %s\n", (ID[i][0]), (ID[i][1]), (ID[i][2]), ID[i][3], ID[i][4]);
+    guardarNombresArchivo(nombreArchivo, ID);
+    fclose(archivo);
     printf("\n");
     menu = 0;
     return menu;
 }
 
-int modificarProductos(FILE *archivo, char ID[7][5][50])
+int modificarProductos(FILE *archivo, char ID[6][5][50], char nombreArchivo[])
 {
-    int cambio, var1, var2, menu;
+    int var1, menu;
 
-    printf("Elegir accion a realizar.\n");
-    printf("1.-Modificar producto                                         2.-Eliminar producto \n");
-    scanf("%d", &cambio);
+    printf("Ingrese el ID del producto a modificar:\n");
+    scanf("%d", &var1);
+    var1 = var1 - 1;
 
-    switch (cambio)
+    fseek(archivo, var1 * (sizeof(float) * 3 + sizeof(char) * 30), SEEK_SET);
+    fscanf(archivo, "%f %f %f %s %s\n", &ID[var1][0], &ID[var1][1], &ID[var1][2], ID[var1][3], ID[var1][4]);
+
+    printf("Ingrese el nuevo nombre del producto:");
+    scanf("%s", ID[var1][1]);
+    printf("Ingrese la nueva descripcion del producto:");
+    scanf("%s", ID[var1][2]);
+    printf("Ingrese la cantidad de productos:");
+    scanf("%s", ID[var1][3]);
+    printf("Ingrese el nuevo precio del producto:");
+    scanf("%s", ID[var1][4]);
+
+    fseek(archivo, var1 * (sizeof(float) * 3 + sizeof(char) * 30), SEEK_SET);
+    fprintf(archivo, "%s %s %s %s %s\n", ID[var1][0], ID[var1][1], ID[var1][2], ID[var1][3], ID[var1][4]);
+    guardarNombresArchivo(nombreArchivo, ID);
+    fclose(archivo);
+    menu = 0;
+    return menu;
+}
+int eliminarProducto(FILE *archivo, char ID[6][5][50], int var, char nombreArchivo)
+{
+    printf("------ ELIMINAR PRODUCTO ------\n");
+    int var1, var2;
+
+    printf("Ingrese el ID del producto a eliminar:\n");
+    scanf("%d", &var1);
+    fflush(stdin);
+    var1 = var1 - 1;
+    var2 = 0;
+
+    printf("Presione 1 para confirmar o 2 para cancelar la eliminacion del producto con ID %d\n", var1 + 1);
+    scanf("%d", &var2);
+
+     if (var2 == 1)
+{
+    for (int i = var1; i < var; i++)
     {
-    case 1:
-        printf("Ingrese el ID del producto a modificar:\n");
-        scanf("%d", &var1);
-        var1 = var1 - 1;
+        strcpy(ID[i][0], ID[i + 1][0]);
+        strcpy(ID[i][1], ID[i + 1][1]);
+        strcpy(ID[i][2], ID[i + 1][2]);
+        strcpy(ID[i][3], ID[i + 1][3]);
+        strcpy(ID[i][4], ID[i + 1][4]);
+    }
+    var--;
 
-        fseek(archivo, var1 * (sizeof(float) * 3 + sizeof(char) * 30), SEEK_SET);
-        fscanf(archivo, "%f %f %f %s %s\n", &ID[var1][0], &ID[var1][1], &ID[var1][2], ID[var1][3], ID[var1][4]);
-
-        printf("Ingrese el nuevo nombre del producto:");
-        scanf("%s", ID[var1][1]);
-        printf("Ingrese la nueva descripcion del producto:");
-        scanf("%s", ID[var1][2]);
-        printf("Ingrese la cantidad de productos:");
-        scanf("%s", ID[var1][3]); 
-        printf("Ingrese el nuevo precio del producto:");
-        scanf("%s", ID[var1][4]); 
-
-        fseek(archivo, var1 * (sizeof(float) * 3 + sizeof(char) * 30), SEEK_SET);
-        fprintf(archivo, " %s %s %s %s\n", ID[var1][1],ID[var1][2], ID[var1][3], ID[var1][4]);
-
-        menu = 0;
-        break;
-    case 2:
-        printf("Ingrese el ID del producto a eliminar:\n");
-        scanf("%d", &var1);
-        var1 = var1 - 1;
-        var2 = 0;
-        printf("Presione 1 para confirmar o 2 para cancelar la eliminacion del producto con ID %d\n", var1 + 1);
-        scanf("%d", &var2);
-        if (var2 == 1)
-        {
-            fseek(archivo, var1 * (sizeof(float) * 3 + sizeof(char) * 30), SEEK_SET);
-            fprintf(archivo, "%s 0 0 Vacio Vacio\n",ID[var1][0]);
-        }
-        printf("\n");
-        menu = 0;
-        break;
-    default:
-        printf("No se ingreso ninguna opcion valida\n");
-        menu = 0;
-        break;
+    fseek(archivo, 0, SEEK_SET);
+    for (int i = 0; i < var; i++)
+    {
+        fprintf(archivo, "%s %s %s %s %s\n", ID[i][0], ID[i][1], ID[i][2], ID[i][3], ID[i][4]);
     }
 
-    return menu;
+    printf("Producto eliminado exitosamente.\n\n");
+}
+else
+{
+    printf("Eliminación cancelada.\n\n");
+}
+
+guardarNombresArchivo(nombreArchivo, ID);
+
+int menu = 0;
+return menu;
 }
